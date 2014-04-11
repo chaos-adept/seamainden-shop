20181e05219dff65aba3cedb159cb4e4c38d8add
chaos-adept <julia.rykovanova@gmail.com>
2014-04-11 08:51:20 +0400

    #SH-3 add authorization support by using passportjs / localonly

--- web-site/app.js
+++ web-site/app.js
@@ -1,15 +1,23 @@
+var conf = require('config');
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var level = require('level');
+var UserApi = require('./userApi/userApi');
 var passport = require('passport');
+var passportLocalStrategy = require('passport-local').Strategy;
+var LeveldbStore = require('connect-leveldb')(express);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var userDb = level(conf.levelDb.baseFolder + '/user', { valueEncoding: 'json' });
+var userApi = UserApi(userDb);
+
+
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,12 +28,93 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.session({
+    store: new LeveldbStore({
+        dbLocation: conf.levelDb.baseFolder+"/sessions",
+        ttl: 60 * 60 * 2
+    }),
+    secret: 'foobarbaz'
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/', routes);
-app.use('/users', users);
+
+app.use(app.router);
+
+// ROUTINES
+
+app.get('/', function(req, res) {
+    console.log("req.user " + req.user);
+    var user = req.user ? req.user : {displayName: "Guest", isGuest:true};
+  res.render('index', { title: 'Express', user:user  });
+});
+
+
+app.post('/users/auth/register', function (req, res) {
+    console.log('new user registers', req.body);
+    req.body.provider = 'local';
+    req.body.id = req.body.login;
+    req.body.displayName = req.body.login;
+    userApi.storeUser(req.body, function (error, user) {
+        if (error) {
+            res.send(500, 'Error registering user');
+        } else {
+            req.login(user, function (error) {
+                if (error) { res.send(500, error); }
+                else { res.redirect('/'); }
+            });
+        }
+    });
+});
+
+app.post('/users/auth/login', 
+    passport.authenticate('local', { successRedirect: '/',
+    failureRedirect: '/error' }));
+
+app.get('/users/auth/logout', function (req, res) {
+
+    req.logout(); 
+    res.redirect('/');
+});
+
+// Настройка стратегий авторизации
+
+passport.serializeUser(userApi.serializeUser);
+passport.deserializeUser(userApi.deserializeUser);
+
+passport.use(new passportLocalStrategy(function (username, password, callback) {
+    console.log('password auth', {username: username, password: password});
+    var profile = {
+        provider: 'local',
+        id: username
+    };
+    var userId = 'local:' + username;
+    userApi.getUser(userId, function (error, user) {
+        if (error) {
+            if (error.name == 'NotFoundError') {
+                callback(null, false, {message: 'Incorrect username'});
+            } else {
+                callback(error);
+            }
+        } else {
+            if (user) {
+                // Пользователь найден, далее проверяем пароль
+                var passwordIsCorrect = userApi.checkPassword(user, password);
+                if (passwordIsCorrect) {
+                    callback(null, user);
+                } else {
+                    callback(null, false, { message: 'Incorrect password'});
+                }
+            } else {
+                callback(null, false, {message: 'Incorrect username'});
+            }
+        }
+
+    })
+}));
+
 
 
 /// catch 404 and forwarding to error handler
